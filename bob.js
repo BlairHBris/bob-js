1bccb4f42f3f6c6a8e655a6446e31ab6f12b7fbc
Blair Bristow <blairhbristow@gmail.com>
2022-04-28 13:27:00 -0400

    Semi solution

--- bob.js
+++ bob.js
@@ -1,1 +1,11 @@
-export function hey() {}
+export function hey(question) {
+    if(question.includes("?") && (question.length > 0) && (question.match(/[A-Z]/g) || []).length < 2) {
+        return "Sure."
+    } else if(question.includes("?") && (question.length > 0) && (question.match(/[A-Z]/g) || []).length >= 2) {
+        return "Whoa, chill out!"
+    } else if(question.length = 0) {
+        return "Fine. Be that way!"
+    } else {
+        return "Whatever."
+    }
+}
